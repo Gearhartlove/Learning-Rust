f9c519b91e0fdcc1992b2159053b5d6994f5811d
Gearhartlove <finley@kristoff.codes>
2021-09-07 19:59:50 -0600

    Sandbox directory created

---
+++ Sandbox/target/debug/deps/Sandbox-22f6863382a4406a.d
@@ -0,0 +1,5 @@
+D:\krist\github\Learning-Rust\Sandbox\target\debug\deps\Sandbox-22f6863382a4406a.rmeta: src\main.rs
+
+D:\krist\github\Learning-Rust\Sandbox\target\debug\deps\Sandbox-22f6863382a4406a.d: src\main.rs
+
+src\main.rs:
--- temperature_converter/target/debug/deps/temperature_converter.d
+++
@@ -1,5 +0,0 @@
-D:\krist\github\Learning-Rust\temperature_converter\target\debug\deps\temperature_converter.exe: src\main.rs
-
-D:\krist\github\Learning-Rust\temperature_converter\target\debug\deps\temperature_converter.d: src\main.rs
-
-src\main.rs:
