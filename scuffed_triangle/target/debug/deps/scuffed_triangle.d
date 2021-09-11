4f0ab85cbaf880eb7347d735a2be421e7ffc3da3
Gearhartlove <finley@kristoff.codes>
2021-09-11 16:17:45 -0600

    Triangle file name change

---
+++ scuffed_triangle/target/debug/deps/scuffed_triangle.d
@@ -0,0 +1,5 @@
+D:\krist\github\Learning-Rust\scuffed_triangle\target\debug\deps\scuffed_triangle.exe: src\main.rs
+
+D:\krist\github\Learning-Rust\scuffed_triangle\target\debug\deps\scuffed_triangle.d: src\main.rs
+
+src\main.rs:
---
+++ scuffed_triangle/target/debug/scuffed_triangle.d
@@ -0,0 +1,1 @@
+D:\krist\github\Learning-Rust\scuffed_triangle\target\debug\scuffed_triangle.exe: D:\krist\github\Learning-Rust\scuffed_triangle\src\main.rs
